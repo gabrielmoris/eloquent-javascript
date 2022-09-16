3a64ac2c1028bb694127318da98d50744c468658
Gabriel C Moris <gabrieltrompeta@gmail.com>
2022-09-16 20:49:35 +0200

    bean counting

--- functions.js
+++ functions.js
@@ -21,7 +21,32 @@ function isEven(number) {
   }
 }
 
-console.log("-4", isEven(-4));
-console.log("4", isEven(4));
-console.log("50", isEven(50));
-console.log("75", isEven(75));
+// console.log("-4", isEven(-4));
+// console.log("4", isEven(4));
+// console.log("50", isEven(50));
+// console.log("75", isEven(75));
+
+//Bean Counting
+
+function countBs(string) {
+  let counter = 0;
+  for (let i = 0; i < string.length; i++) {
+    if (string[i] === "B") {
+      counter++;
+    }
+  }
+  return counter;
+}
+
+// console.log(countBs("Bubu Bebe biBi"));
+
+function countChar(string, char) {
+  let counter = 0;
+  for (let i = 0; i < string.length; i++) {
+    if (string[i] === char) {
+      counter++;
+    }
+  }
+  return counter;
+}
+// console.log(countChar("Bubu Bebe biBi", "e"));
