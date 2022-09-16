fbf0573e7e5f76ff661c42ae7c83cbddc65e351d
Gabriel C Moris <gabrieltrompeta@gmail.com>
2022-09-16 20:34:56 +0200

    recursion

--- functions.js
+++ functions.js
@@ -7,4 +7,21 @@ function minimum(num1, num2) {
   }
 }
 
-//Recursion
+// Recursion;
+function isEven(number) {
+  if (number >= 2) {
+    number = number - 2;
+    return isEven(number);
+  } else if (number === 1) {
+    return false;
+  } else if (number === 0) {
+    return true;
+  } else {
+    return isEven(-number);
+  }
+}
+
+console.log("-4", isEven(-4));
+console.log("4", isEven(4));
+console.log("50", isEven(50));
+console.log("75", isEven(75));
