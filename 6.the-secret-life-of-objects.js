c65abf1e0fca22d9bf2a6f09e4fbc968280c9878
Gabriel C Moris <gabrieltrompeta@gmail.com>
2022-10-10 22:44:18 +0200

    groups

--- 6.the-secret-life-of-objects.js
+++ 6.the-secret-life-of-objects.js
@@ -25,3 +25,42 @@ class Vec {
 // // → Vec{x: -1, y: -1}
 // console.log(new Vec(3, 4).length);
 // // → 5
+
+//Groups
+class Group {
+  constructor() {
+    this.collection = [];
+  }
+  // this creates a new group with the items in the object || arr
+  static from(value) {
+    let group = new Group();
+    for (let item of value) {
+      group.add(item);
+    }
+    return group;
+  }
+  add(value) {
+    this.collection.push(value);
+  }
+  delete(value) {
+    this.collection = this.collection.filter((item) => item != value);
+  }
+  has(value) {
+    for (let i = 0; i < this.collection.length; i++) {
+      if (this.collection[i] === value) {
+        return true;
+      }
+      return false;
+    }
+  }
+}
+
+// let group = Group.from([10, 20]);
+// console.log(group.has(10));
+// // → true
+// console.log(group.has(30));
+// // → false
+// group.add(10);
+// group.delete(10);
+// console.log(group.has(10));
+// // → false
