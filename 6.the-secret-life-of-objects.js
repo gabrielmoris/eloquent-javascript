a790336da2d53064449fbcdceaf3c680b71ac0d3
Gabriel C Moris <gabrieltrompeta@gmail.com>
2022-10-10 20:43:51 +0200

    A vector type

---
+++ 6.the-secret-life-of-objects.js
@@ -0,0 +1,27 @@
+// A vector type
+class Vec {
+  constructor(x, y) {
+    this.x = x;
+    this.y = y;
+  }
+  plus(className) {
+    this.x = this.x + className.x;
+    this.y = this.y + className.y;
+    return this;
+  }
+  minus(className) {
+    this.x = this.x - className.x;
+    this.y = this.y - className.y;
+    return this;
+  }
+  get length() {
+    return Math.sqrt(this.x ** 2 + this.y ** 2);
+  }
+}
+
+console.log(new Vec(1, 2).plus(new Vec(2, 3)));
+// → Vec{x: 3, y: 5}
+console.log(new Vec(1, 2).minus(new Vec(2, 3)));
+// → Vec{x: -1, y: -1}
+console.log(new Vec(3, 4).length);
+// → 5
